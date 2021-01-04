3edab77abbefec7c5c2747ed4867126a6bd453f0
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2021-01-04 22:46:32 +0200

    Accept pytest.param() keywords for test cases
    Test cases can now include the `marks=` and `id=` keyword
    arguments. For example:
    
        @pytest.mark.kwparametrize(
            dict(a=1, id="first", marks=[pytest.mark.xfail]),
        )

--- src/pytest_kwparametrize/plugin.py
+++ src/pytest_kwparametrize/plugin.py
@@ -1,20 +1,32 @@
-from inspect import Parameter, signature
-from typing import Any, Dict, Iterable, List, Mapping, Tuple
+"""kwparametrize plugin"""
 
+from inspect import Parameter, signature
+from typing import Any, Callable, Dict, Iterable, List, Mapping, Tuple, Union
+
+import pytest
 from _pytest.python import Metafunc
 from pytest import fail
 
+
+def get_keyword_parameters(function: Callable) -> List[str]:
+    return [
+        param_name
+        for param_name, param in signature(function).parameters.items()
+        if param.kind in [Parameter.POSITIONAL_OR_KEYWORD, Parameter.KEYWORD_ONLY]
+    ]
+
+
 # Keyword argument names accepted by @pytest.mark.parametrize().
-# All other keywords can be used as parameter names for parametrized test cases.
+# These keywords can't be used as parameter names for parametrized test cases.
 # Fish these from Pytest internals to hope for better forward compatibility.
-_pytest_mark_parametrize_params = signature(Metafunc.parametrize).parameters
-_pytest_mark_parametrize_kw_params = [
-    param_name
-    for param_name, param in _pytest_mark_parametrize_params.items()
-    if param.kind in [Parameter.POSITIONAL_OR_KEYWORD, Parameter.KEYWORD_ONLY]
-]
-PARAMETRIZE_KEYWORDS = _pytest_mark_parametrize_kw_params[3:]
 # In Pytest 6.1.2: ["indirect", "ids", "scope", "_param_mark"]
+PARAMETRIZE_KEYWORDS = set(get_keyword_parameters(Metafunc.parametrize)[3:])
+
+# Keyword argument names accepted by `pytest.param(...)`
+# These keywords can't be used as parameter names for parametrized test cases.
+# Fish these from Pytest internals to hope for better forward compatibility.
+# In Pytest 6.1.2: ["marks", "id"]
+PYTEST_PARAM_KEYWORDS = set(get_keyword_parameters(pytest.param))
 
 
 def _get_param(
@@ -39,15 +51,17 @@ def _has_one_iterable(marker_args: List[Any]):
     )
 
 
-def _parse_marker_args(marker_args: List[Any]) -> Tuple[List[Any], List[Any]]:
+def _parse_marker_args(
+    marker_args: Iterable[Union[List[Dict[str, Any]], Dict[str, Any]]]
+) -> List[Any]:
     if _has_one_iterable(marker_args):
         # called like:
         # @pytest.mark.kwparametrize([dict(<case1>), dict(<case2>), ...])
-        testcase_dicts, *parametrize_args = marker_args
-        return testcase_dicts, parametrize_args
+        (testcase_dicts,) = marker_args
+        return testcase_dicts
     # called like:
     # @pytest.mark.kwparametrize(dict(<case1>), dict(<case2>), ...)
-    return marker_args, []
+    return marker_args
 
 
 def _parse_marker_kwargs(kwargs: Dict[str, Any]):
@@ -70,22 +84,49 @@ def _param_dicts_to_tuples(
         param_values = (
             _get_param(name, params, defaults, metafunc) for name in defaults
         )
-        argvalues.append(tuple(param_values))
+        param_keywords = {
+            keyword: value
+            for keyword, value in params.items()
+            if keyword in PYTEST_PARAM_KEYWORDS
+        }
+        argvalues.append(pytest.param(*param_values, **param_keywords))
     return argvalues
+
+
+def kwparametrize(
+    metafunc: Metafunc,
+    *args: Union[List[Dict[str, Any]], Dict[str, Any]],
+    **kwargs: Any,
+) -> None:
+    """foo"""
+    testcase_dicts = _parse_marker_args(args)
+    defaults, parametrize_kwargs = _parse_marker_kwargs(kwargs)
+    for params in testcase_dicts:
+        for name in params:
+            if name not in PYTEST_PARAM_KEYWORDS:
+                defaults.setdefault(name, ...)
+    testcase_tuples = _param_dicts_to_tuples(testcase_dicts, defaults, metafunc)
+    metafunc.parametrize(
+        argnames=list(defaults),
+        argvalues=testcase_tuples,
+        **parametrize_kwargs,
+    )
 
 
 def pytest_generate_tests(metafunc: Metafunc) -> None:
     for marker in metafunc.definition.iter_markers(name="kwparametrize"):
-        testcase_dicts, parametrize_args = _parse_marker_args(marker.args)
-        defaults, parametrize_kwargs = _parse_marker_kwargs(marker.kwargs)
-        for params in testcase_dicts:
-            for name in params:
-                defaults.setdefault(name, ...)
-        testcase_tuples = _param_dicts_to_tuples(testcase_dicts, defaults, metafunc)
-        metafunc.parametrize(
-            list(defaults),
-            testcase_tuples,
-            *parametrize_args,
-            **parametrize_kwargs,
-            _param_mark=marker,
-        )
+        kwparametrize(metafunc, *marker.args, _param_mark=marker, **marker.kwargs)
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers",
+        "kwparametrize(*argvalues):"
+        " call a test function multiple times passing in different arguments in turn."
+        " argvalues items need to be dictionaries."
+        " Example: @kwparametrize(dict(arg1=1), dict(arg1=2))"
+        " would lead to two calls of the decorated test function,"
+        " one with arg1=1 and another with arg1=2."
+        " see https://github.com/akaihola/pytest-kwparametrize"
+        " for more info and examples.",
+    )
